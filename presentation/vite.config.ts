4e1f5709cbb424c6cd5e527bfea5fba8bc03bcba
David Puente <dpuente@dinahosting.com>
2023-11-21 11:29:54 +0000

    build(root): :sparkles: add vite port by enviroment an public host

--- presentation/vite.config.ts
+++ presentation/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 
@@ -22,9 +21,8 @@ export default defineConfig(({ mode }) => {
   }
 
   return {
-    plugins: [react()],
     server: {
-      port: parseInt(processEnvValues['process.env'].CLIENT_PORT || '5173')
+      port: parseInt(processEnvValues['process.env'].PRESENTATION_PORT || '5174')
     }
   }
 })
